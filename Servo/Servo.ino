a271d6d1e17e97875335b2fcc8dfb226775ea319
Priezt <vegetabird@gmail.com>
2012-09-18 18:26:45 +0800

    nfc

---
+++ Servo/Servo.ino
@@ -0,0 +1,24 @@
+#include <Servo.h>
+
+Servo myservo;
+int servoPin = 9; // Set this to the PIN connected to servo's control line
+int val = 0;
+int stepAngle = 1;
+int interval = 10;
+int minAngle = 0;
+int maxAngle = 180;
+
+void setup(){
+  myservo.attach(servoPin);
+}
+
+void loop(){
+  for(val=minAngle;val<=maxAngle;val+=stepAngle){
+    myservo.write(val);
+    delay(interval);
+  }
+  for(val=maxAngle;val>=minAngle;val-=stepAngle){
+    myservo.write(val);
+    delay(interval);
+  }
+}
